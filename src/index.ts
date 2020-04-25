be465e9c68578c08d25261acef2e663bce0e2d67
j- <j@skeoh.com>
2020-04-25 16:50:01 +1000

    Setup locally

--- src/index.ts
+++ src/index.ts
@@ -1,13 +1,17 @@
 import * as http from 'http';
 import { getData } from './kino';
 
-//create a server object:
+const hostname = process.argv[2] || process.env.HOSTNAME || 'localhost';
+const port = Number(process.argv[3] || process.env.PORT || 8080);
+
 http
-  .createServer(async function(req, res) {
+  .createServer(async (_req, res) => {
     const data = await getData();
     res.setHeader('Content-Type', 'application/javascript');
-    res.setHeader('Access-Control-Allow-Origin', 'https://5076q.csb.app');
+    res.setHeader('Access-Control-Allow-Origin', 'http://localhost:3000');
     res.write(JSON.stringify(data, null, 2));
-    res.end(); //end the response
+    res.end();
   })
-  .listen(8080); //the server object listens on port 8080
+  .listen(port, hostname, () => {
+    console.log(`Server listening on http://${hostname}:${port}`);
+  });
--- src/kino.ts
+++ src/kino.ts
@@ -1,17 +1,26 @@
-// import fetch from 'node-fetch';
+import fetch from 'node-fetch';
 import * as fs from 'fs';
 import { JSDOM } from 'jsdom';
 import { scrapeSessionData } from './scrape';
 import { parseSessionData } from './parse';
 
+const IS_DEBUG = process.env.NODE_ENV !== 'production';
+
 export const URL = 'https://www.palacecinemas.com.au/cinemas/the-kino/';
 
 export const getData = async () => {
-  const text = await fs.promises.readFile(__dirname + '/../test/res/2019-09-19.html', 'utf-8');
-  // const res = await fetch(URL);
-  // const text = await res.text();
+  let text: string;
+
+  if (IS_DEBUG) {
+    text = await fs.promises.readFile(__dirname + '/../test/res/2019-09-16.html', 'utf-8');
+  } else {
+    const res = await fetch(URL);
+    text = await res.text();
+  }
+
   const dom = new JSDOM(text, {
     url: URL,
   });
+
   return parseSessionData(scrapeSessionData(dom.window));
 };
