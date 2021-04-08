7f6ad897e264082187c6ab5ab7450ca74f900d07
j- <j@skeoh.com>
2021-04-08 20:07:56 +1000

    Change parse lib to read from stdin

--- lib/parse.ts
+++ lib/parse.ts
@@ -1,36 +1,26 @@
-import * as fs from 'fs';
-import { resolve } from 'path';
 import { JSDOM } from 'jsdom';
 import { scrapeSessionData } from '../src/scrape';
 import { parseSessionData } from '../src/parse';
 
-if (process.argv.length < 3) {
-  console.error('Usage: npm run -s parse -- <filename>');
-  process.exit(1);
-}
-
-const filename = process.argv[2];
-const path = resolve(process.cwd(), filename);
-
-if (!fs.existsSync(path)) {
-  console.error(`Error: could not find file at path "${path}"`);
-  process.exit(1);
-}
-
 let text: string;
 
-try {
-  text = fs.readFileSync(path, 'utf-8');
-} catch (err) {
-  console.error(`Error reading file: "${err.message}"`);
-  process.exit(1);
-}
+process.stdin.resume();
+process.stdin.setEncoding('utf8');
 
-const dom = new JSDOM(text, {
-  url: 'https://www.palacecinemas.com.au/cinemas/the-kino/',
+process.stdin.on('data', (chunk) => {
+  text += chunk;
 });
 
-const result = parseSessionData(scrapeSessionData(dom.window));
-const string = JSON.stringify(result, null, 2);
-
-process.stdout.write(string);
+process.stdin.on('end', () => {
+  try {
+    const dom = new JSDOM(text, {
+      url: 'https://www.palacecinemas.com.au/cinemas/the-kino/',
+    });
+    const result = parseSessionData(scrapeSessionData(dom.window));
+    const string = JSON.stringify(result, null, 2);
+    process.stdout.write(string);
+  } catch (err) {
+    console.error(`Error parsing file: "${err.message}"`);
+    process.exit(1);
+  }
+});
