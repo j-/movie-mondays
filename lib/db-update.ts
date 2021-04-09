d51874e95007735f06ec8c3409b086a294480e0a
j- <j@skeoh.com>
2021-04-09 13:21:01 +1000

    Add meaningful errors to db update script

--- lib/db-update.ts
+++ lib/db-update.ts
@@ -1,5 +1,5 @@
 import { resolve } from 'path';
-import fetch from 'node-fetch';
+import fetch, { Response } from 'node-fetch';
 import { JSDOM } from 'jsdom';
 import * as Database from 'better-sqlite3';
 import { scrapeSessionData } from '../src/scrape';
@@ -17,12 +17,50 @@ const path = resolve(process.cwd(), filename);
 const URL = 'https://www.palacecinemas.com.au/cinemas/the-kino/';
 
 (async () => {
-  const res = await fetch(URL);
-  const text = await res.text();
-  const dom = new JSDOM(text, {
-    url: URL,
-  });
-  const data = parseSessionData(scrapeSessionData(dom.window));
-  const db = new Database(path);
-  await insertAllEntities(db, data);
+  let res: Response;
+
+  try {
+    res = await fetch(URL);
+  } catch (err) {
+    console.error(`Error fetching page data: "${err.message}"`);
+    process.exit(1);
+  }
+
+  let dom: JSDOM;
+
+  try {
+    const text = await res.text();
+    dom = new JSDOM(text, {
+      url: URL,
+    });
+  } catch (err) {
+    console.error(`Error parsing response document: "${err.message}"`);
+    process.exit(1);
+  }
+
+  let sessionData: ReturnType<typeof scrapeSessionData>;
+
+  try {
+    sessionData = scrapeSessionData(dom.window);
+  } catch (err) {
+    console.error(`Error scraping session data: "${err.message}"`);
+    process.exit(1);
+  }
+
+  let data: ReturnType<typeof parseSessionData>;
+
+  try {
+    data = parseSessionData(sessionData);
+  } catch (err) {
+    console.error(`Error parsing session data: "${err.message}"`);
+    process.exit(1);
+  }
+
+  try {
+    const db = new Database(path);
+    await insertAllEntities(db, data);
+  } catch (err) {
+    console.error(`Error inserting entity data: "${err.message}"`);
+    process.exit(1);
+  }
 })();
