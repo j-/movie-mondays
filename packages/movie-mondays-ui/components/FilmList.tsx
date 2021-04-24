8e06010a5f101c0f93a790de1aabb5528db92c2c
j- <j@skeoh.com>
2021-04-24 17:31:56 +1000

    Add query param to film list

--- packages/movie-mondays-ui/components/FilmList.tsx
+++ packages/movie-mondays-ui/components/FilmList.tsx
@@ -1,4 +1,5 @@
 import * as React from 'react';
+import { useRouter } from 'next/router';
 import { Film } from 'movie-mondays-data';
 import FuzzySearch from 'fuzzy-search';
 import { sortFilm } from '../sort';
@@ -8,13 +9,35 @@ type Props = {
   films: Film[];
 }
 
+export const QUERY_FIELD = 'q';
+
 const FilmList: React.FC<Props> = ({ films }) => {
-  const [search, setSearch] = React.useState('');
+  const router = useRouter();
+  const params = new URLSearchParams(router.asPath.replace(/^.*?(\?|$)/, ''));
+  const searchParam = params.get(QUERY_FIELD);
+  const [search, setSearch] = React.useState(searchParam ?? '');
   const handleChangeSearch: React.ChangeEventHandler<HTMLInputElement> = (e) => {
-    setSearch(e.currentTarget.value);
+    const search = e.currentTarget.value;
+    setSearch(search);
+    const url = router.pathname;
+    const options = {
+      query: {
+        [QUERY_FIELD]: search,
+      },
+    };
+    if (searchParam) {
+      router.replace(url, options);
+    } else {
+      router.push(url, options);
+    }
   };
   const searcher = React.useMemo(() => new FuzzySearch(films, ['title']), [films]);
   const result = React.useMemo(() => searcher.search(search), [searcher, search]);
+  React.useEffect(() => {
+    if (searchParam !== search) {
+      setSearch(searchParam ?? '');
+    }
+  }, [searchParam]);
   return (
     <div>
       <input type="text" value={search} onChange={handleChangeSearch} />
