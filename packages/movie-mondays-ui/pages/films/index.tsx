e67e83362c8a931c322a0dc2760dce7fce1443be
j- <j@skeoh.com>
2021-04-24 15:05:31 +1000

    Query for films

--- packages/movie-mondays-ui/pages/films/index.tsx
+++ packages/movie-mondays-ui/pages/films/index.tsx
@@ -1,23 +1,25 @@
 import { GetStaticProps } from 'next';
 import Link from 'next/link';
-
-import { User } from '../../interfaces';
-import { sampleUserData } from '../../utils/sample-data';
+import Database from 'better-sqlite3';
+import { Film, getAllFilms} from 'movie-mondays-data';
 import Layout from '../../components/Layout';
-import List from '../../components/List';
 
 type Props = {
-  items: User[]
+  items: Film[]
 }
 
-const WithStaticProps = ({ items }: Props) => (
-  <Layout title="Users List | Next.js + TypeScript Example">
-    <h1>Users List</h1>
+const WithStaticProps: React.FC<Props> = ({ items }) => (
+  <Layout title="Films List | Next.js + TypeScript Example">
+    <h1>Films List</h1>
     <p>
       Example fetching data from inside <code>getStaticProps()</code>.
     </p>
-    <p>You are currently on: /users</p>
-    <List items={items} />
+    <p>You are currently on: /films</p>
+    <ul>
+      {items.map((item) => (
+        <li key={item.id}>{item.title}</li>
+      ))}
+    </ul>
     <p>
       <Link href="/">
         <a>Go home</a>
@@ -26,11 +28,9 @@ const WithStaticProps = ({ items }: Props) => (
   </Layout>
 );
 
-export const getStaticProps: GetStaticProps = async () => {
-  // Example for including static props in a Next.js function component page.
-  // Don't forget to include the respective types for any props passed into
-  // the component.
-  const items: User[] = sampleUserData;
+export const getStaticProps: GetStaticProps<Props> = async () => {
+  const db = new Database('../../database.sqlite');
+  const items: Film[] = await getAllFilms(db);
   return { props: { items } };
 };
 
