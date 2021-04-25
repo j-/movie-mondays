d23dd005ad6e4f47fc2e54b22e550733c6bd0999
j- <j@skeoh.com>
2021-04-25 13:37:41 +1000

    Add container to layout

--- packages/movie-mondays-ui/components/Layout.tsx
+++ packages/movie-mondays-ui/components/Layout.tsx
@@ -1,6 +1,7 @@
 import React, { ReactNode } from 'react';
 import Link from 'next/link';
 import Head from 'next/head';
+import styles from './Layout.module.css';
 
 type Props = {
   children?: ReactNode;
@@ -8,34 +9,36 @@ type Props = {
 }
 
 const Layout: React.FC<Props> = ({ children, title = 'Movie Mondays' }) => (
-  <div>
+  <>
     <Head>
       <title>{title}</title>
       <meta charSet="utf-8" />
       <meta name="viewport" content="initial-scale=1.0, width=device-width" />
     </Head>
-    <header>
-      <nav>
-        <Link href="/">
-          <a>Home</a>
-        </Link>{' '}
-        |{' '}
-        <Link href="/about">
-          <a>About</a>
-        </Link>{' '}
-        |{' '}
-        <Link href="/films">
-          <a>Films List</a>
-        </Link>{' '}
-        | <a href="/api/films">Films API</a>
-      </nav>
-    </header>
-    {children}
-    <footer>
-      <hr />
-      <span>I'm here to stay (Footer)</span>
-    </footer>
-  </div>
+    <div className={styles.layout}>
+      <header>
+        <nav>
+          <Link href="/">
+            <a>Home</a>
+          </Link>{' '}
+          |{' '}
+          <Link href="/about">
+            <a>About</a>
+          </Link>{' '}
+          |{' '}
+          <Link href="/films">
+            <a>Films List</a>
+          </Link>{' '}
+          | <a href="/api/films">Films API</a>
+        </nav>
+      </header>
+      {children}
+      <footer>
+        <hr />
+        <span>I'm here to stay (Footer)</span>
+      </footer>
+    </div>
+  </>
 );
 
 export default Layout;
--- packages/movie-mondays-ui/tailwind.config.js
+++ packages/movie-mondays-ui/tailwind.config.js
@@ -6,6 +6,16 @@ module.exports = {
   darkMode: false, // or 'media' or 'class'
   theme: {
     extend: {},
+    container: {
+      center: true,
+      screens: {
+        'sm': '100%',
+        'md': '100%',
+        'lg': '100%',
+        'xl': '600px',
+        '2xl': '800px',
+      },
+    },
   },
   variants: {
     extend: {},
