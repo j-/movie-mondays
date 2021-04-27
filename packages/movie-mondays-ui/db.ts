632c19e2fe8b51f4cfbac6a58dc6806eb871cc01
j- <j@skeoh.com>
2021-04-27 20:51:43 +1000

    Add env files with DB file var

--- packages/movie-mondays-ui/db.ts
+++ packages/movie-mondays-ui/db.ts
@@ -4,7 +4,10 @@ let db: Database.Database;
 
 const getDatabase = () => {
   if (!db) {
-    db = new Database('../../database.sqlite');
+    if (!process.env.DB_FILE) {
+      throw new Error('DB_FILE env var not set');
+    }
+    db = new Database(process.env.DB_FILE);
   }
   return db;
 };
