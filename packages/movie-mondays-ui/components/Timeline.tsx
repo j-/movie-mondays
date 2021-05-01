377fb95358c6fae8d6587af8473348e67b8b14a0
j- <j@skeoh.com>
2021-05-01 22:45:10 +1000

    Change timeline size and alignment

--- packages/movie-mondays-ui/components/Timeline.tsx
+++ packages/movie-mondays-ui/components/Timeline.tsx
@@ -1,3 +1,4 @@
+import classNames from 'classnames';
 import { addTime, subtractTime, formatStringAsTime } from '../utils';
 
 type Props = {
@@ -24,29 +25,29 @@ const Timeline: React.FC<Props> = ({
 
   return (
     <ul className="w-full grid grid-cols-4">
-      <li className={now > timeDoors ? 'opacity-50' : ''}>
-        <span>
+      <li className={classNames(now > timeDoors && 'opacity-50', 'text-center')}>
+        <span className="text-lg">
           {formatStringAsTime(timeDoors)}
         </span><br />
-        <span>Doors open<br />(Grab snacks)</span>
+        <span className="text-sm">Doors open<br />(Grab snacks)</span>
       </li>
-      <li className={now > timeTrailers ? 'opacity-50' : ''}>
-        <span>
+      <li className={classNames(now > timeTrailers && 'opacity-50', 'text-center')}>
+        <span className="text-lg">
           {formatStringAsTime(timeTrailers)}
         </span><br />
-        <span>Trailers start<br />({doorsMinutes} min. later)</span>
+        <span className="text-sm">Trailers start<br />({doorsMinutes} min. later)</span>
       </li>
-      <li className={now > timeStart ? 'opacity-50' : ''}>
-        <span>
+      <li className={classNames(now > timeStart && 'opacity-50', 'text-center')}>
+        <span className="text-lg">
           {formatStringAsTime(timeStart)}
         </span><br />
-        <span>Movie starts<br />({trailersMinutes} min. later)</span>
+        <span className="text-sm">Movie starts<br />({trailersMinutes} min. later)</span>
       </li>
-      <li className={now > timeFinish ? 'opacity-50' : ''}>
-        <span>
+      <li className={classNames(now > timeFinish && 'opacity-50', 'text-center')}>
+        <span className="text-lg">
           {formatStringAsTime(timeFinish)}
         </span><br />
-        <span>Movie ends<br />({runtimeMinutes} min. later)</span>
+        <span className="text-sm">Movie ends<br />({runtimeMinutes} min. later)</span>
       </li>
     </ul>
   );
